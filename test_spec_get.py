7d52fe874b1a6d6c2dae1ef974ab71e3c0f17992
0dj3 <xaryshal@gmail.com>
2022-05-04 16:43:26 +0900

    dobavil

---
+++ test_spec_get.py
@@ -0,0 +1,14 @@
+from main import Specialization
+from classes import institute
+import unittest
+
+
+class TestGetSpecialization(unittest.TestCase):
+
+    sp = Specialization("ФИИТ")
+    inst = institute.Institute()
+    inst.add_spec(sp)
+
+    def test_one(self):  # Корректное количество строк
+        self.assertEqual(len(self.inst.get_spec(self.sp.name)), 1)
+
